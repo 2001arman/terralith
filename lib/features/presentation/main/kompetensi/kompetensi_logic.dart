d7c0fa243e268bf428ece9decdf2fdd50acecd30
arman <2001armanmaulana@gmail.com>
2025-07-05 14:06:41 +0700

    adj: cp atp content

--- lib/features/presentation/main/kompetensi/kompetensi_logic.dart
+++ lib/features/presentation/main/kompetensi/kompetensi_logic.dart
@@ -7,7 +7,7 @@ class KompetensiLogic extends GetxController {
 <ol>
   <li>Memahami dan menganalisis proses geosfer, termasuk dinamika litosfer dan dampaknya terhadap kehidupan.</li>
   <li>Mengidentifikasi dan menjelaskan fenomena geologi serta upaya mitigasi bencana.</li>
-  <li>Menerapkan sikap peduli dan tanggung jawab terhadap lingkungan sekitarnya.</li>
+  <li>Menunjukkan sikap peduli, bertanggung jawab, dan kesiapsiagaan dalam menghadapi risiko bencana geologi di lingkungan sekitarnya.</li>
 </ol>
 
 <h1>Tujuan Pembelajaran</h1>
@@ -17,6 +17,14 @@ class KompetensiLogic extends GetxController {
   <li>Mengidentifikasi proses endogen dan eksogen yang membentuk permukaan bumi.</li>
   <li>Menganalisis dampak dinamika litosfer terhadap kehidupan manusia.</li>
   <li>Mengembangkan sikap peduli terhadap mitigasi bencana yang berkaitan dengan dinamika litosfer.</li>
+</ol>
+
+<h1>Tujuan Pengembangan Aplikasi Terralith</h1>
+<p>Aplikasi Terralith dikembangkan sebagai media pembelajaran mandiri untuk:</p>
+<ol>
+  <li>Membantu peserta didik memahami dinamika litosfer dan dampaknya terhadap kehidupan.</li>
+  <li>Meningkatkan kesadaran siswa terhadap risiko bencana geologi melalui pembelajaran interaktif.</li>
+  <li>Menumbuhkan sikap peduli, bertanggung jawab, dan kesiapsiagaan siswa dalam menghadapi potensi bencana di lingkungan sekitarnya.</li>
 </ol>
 
 <h1>Profil Pelajar Pancasila</h1>
